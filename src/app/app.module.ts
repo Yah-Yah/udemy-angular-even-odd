07f55a8c3771a6b0261d22fd49891cb597435aeb
Janina <janeczka@gmail.com>
2019-12-22 17:58:35 +0000

    Add Odd numbers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent
+    GameControlComponent,
+    OddComponent
   ],
   imports: [
     BrowserModule
